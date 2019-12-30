8d8d7448ca88039cd3dfdb8d6ee580f6064f5fb8
schulle <cschult@devmem.de>
2019-12-30 09:24:09 +0100

    get result linewise

--- rs.go
+++ rs.go
@@ -2,33 +2,46 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
 )
 
-func rs() {
-	args := []string{"-t", "-b", "dir:/home/schulle/ownCloud/rezepte"}
-	fmt.Print("Enter search: ")
-
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	search := scanner.Text()
-
-	args = append(args, search)
-	cmd := exec.Command("recoll", args...)
-
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("%s", out.String())
-}
-
 func main() {
-	rs()
+	// docker build current directory
+	cmdName := "recoll"
+	cmdArgs := []string{"-c", "/home/schulle/.config/recoll", "-t", "-b", "dir:/home/schulle/ownCloud/rezepte"}
+	// cmdArgs := []string{"build", "."}
+
+	fmt.Print("Enter search: ")
+	searchTerm := bufio.NewScanner(os.Stdin)
+	searchTerm.Scan()
+	search := searchTerm.Text()
+	cmdArgs = append(cmdArgs, search)
+
+	cmd := exec.Command(cmdName, cmdArgs...)
+	cmdReader, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error creating StdoutPipe for Cmd", err)
+		os.Exit(1)
+	}
+
+	scanner := bufio.NewScanner(cmdReader)
+	go func() {
+		for scanner.Scan() {
+			fmt.Printf("%s\n", scanner.Text())
+		}
+	}()
+
+	err = cmd.Start()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error starting Cmd", err)
+		os.Exit(1)
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error waiting for Cmd", err)
+		os.Exit(1)
+	}
 }
