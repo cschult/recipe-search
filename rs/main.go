ef87bc7c1139972d612b7219bf36cf7cbe011442
schulle <cschult@devmem.de>
2019-12-30 13:56:01 +0100

    add func args

--- rs/main.go
+++ rs/main.go
@@ -7,17 +7,30 @@ import (
 	"os/exec"
 )
 
+// add search words to list of arguments
+// get from command line or ask at runtime
+func args (cmdargs []string) []string {
+	argsWithoutProg := os.Args[1:]
+	if len(argsWithoutProg) == 0 {
+		fmt.Print("Enter search: ")
+		searchTerm := bufio.NewScanner(os.Stdin)
+		searchTerm.Scan()
+		search := searchTerm.Text()
+		// fmt.Printf("%s\n", search)
+		cmdargs = append(cmdargs, search)
+		return cmdargs
+	} else {
+		cmdargs = append(cmdargs, argsWithoutProg...)
+		return cmdargs
+	}
+}
+
 func Search() {
 	cmdName := "recoll"
+	// needed arguments to call recoll for my recipe collection
 	cmdArgs := []string{"-c", "/home/schulle/.config/recoll", "-t", "-b", "dir:/home/schulle/ownCloud/rezepte"}
 
-	fmt.Print("Enter search: ")
-	searchTerm := bufio.NewScanner(os.Stdin)
-	searchTerm.Scan()
-	search := searchTerm.Text()
-	// fmt.Printf("%s\n", search)
-	cmdArgs = append(cmdArgs, search)
-
+	cmdArgs = args(cmdArgs)
 	cmd := exec.Command(cmdName, cmdArgs...)
 	cmdReader, err := cmd.StdoutPipe()
 	if err != nil {
@@ -44,6 +57,7 @@ func Search() {
 		fmt.Fprintln(os.Stderr, "Error waiting for Cmd", err)
 		os.Exit(1)
 	}
+	// todo: make results viewable
 	for i, v := range result {
 		fmt.Println(i, v)
 	}
